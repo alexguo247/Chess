b5637430235f1447b6741426df14d4d090037ac1
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2022-07-16 19:19:54 -0400

    chess game changes

--- chessgame.h
+++ chessgame.h
@@ -1,15 +1,14 @@
 #include "board.h"
 
-class Chessgame
-{
+class Chessgame {
     Board board;
     Player p1;
     Player p2;
     Colour turn;
 
-public:
-    void game(string, string);
-    void move(string, string);
-    void resign();
-    void setup();
+    public:
+        void game(string, string);
+        void move(string, string);
+        void resign();
+        void setup();
 };
