6c0a6ff79be1d740aed89b90e8f625df7fd50a98
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2022-07-16 19:00:49 -0400

    added command line input and basic chess game/board classes

---
+++ chessgame.h
@@ -0,0 +1,15 @@
+#include "board.h"
+
+class Chessgame
+{
+    Board board;
+    Player p1;
+    Player p2;
+    Colour turn;
+
+public:
+    void game(string, string);
+    void move(string, string);
+    void resign();
+    void setup();
+};
---
+++ main.cc
@@ -0,0 +1,41 @@
+#include "chessgame.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Chessgame *chess;
+    string cmd;
+
+    while (cin >> cmd)
+    {
+        if (cmd == "game")
+        {
+            string p1;
+            string p2;
+            cin >> p1;
+            cin >> p2;
+            chess->game(p1, p2);
+        }
+        else if (cmd == "resign")
+        {
+            chess->resign();
+        }
+        else if (cmd == "move")
+        {
+            string start;
+            string end;
+            chess->move(start, end);
+        }
+        else if (cmd == "setup")
+        {
+            chess->setup();
+        }
+        else
+        {
+            cout << "Not a valid command" << endl;
+        }
+    }
+    return 0;
+}
