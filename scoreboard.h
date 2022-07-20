50b29447dd7da2e50c47c8607ff2ee9fcad1fdee
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2022-07-20 10:40:10 -0400

    chess implementation changes

--- scoreboard.h
+++ scoreboard.h
@@ -1,1 +1,11 @@
-class Scoreboard {}
+#include "colour.h"
+
+class Scoreboard {
+    int p1Score;
+    int p2Score;
+
+    public:
+        Scoreboard();
+        void print();
+        void incrementScore(Colour);
+};
