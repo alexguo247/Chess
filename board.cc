797653e9416cf7e02b966d6b281d9b5267a8c9be
Alex Guo <a35guo@uwaterloo.ca>
2022-07-22 21:34:04 -0400

    En Passant Check

--- board.cc
+++ board.cc
@@ -26,7 +26,7 @@ Board::Board()
     }
 }
 
-Piece* Board::getPiece(int row, int col)
+Piece *Board::getPiece(int row, int col)
 {
     return grid[row][col];
 }
@@ -63,26 +63,27 @@ void Board::setPiece(Piece *piece, int row, int col)
     Type t = piece->getType();
     Colour c = piece->getColour();
 
-    switch (t) {
-        case Type::KING:
-            grid[row][col] = new King(c, row, col);
-        case Type::BISHOP:
-            grid[row][col] = new Bishop(c, row, col);
-        case Type::ROOK:
-            grid[row][col] = new Rook(c, row, col);
-        case Type::KNIGHT:
-            grid[row][col] = new Knight(c, row, col);
-        case Type::QUEEN:
-            grid[row][col] = new Queen(c, row, col);
-        case Type::PAWN:
-            grid[row][col] = new Pawn(c, row, col, true);
+    switch (t)
+    {
+    case Type::KING:
+        grid[row][col] = new King(c, row, col);
+    case Type::BISHOP:
+        grid[row][col] = new Bishop(c, row, col);
+    case Type::ROOK:
+        grid[row][col] = new Rook(c, row, col);
+    case Type::KNIGHT:
+        grid[row][col] = new Knight(c, row, col);
+    case Type::QUEEN:
+        grid[row][col] = new Queen(c, row, col);
+    case Type::PAWN:
+        grid[row][col] = new Pawn(c, row, col, true, false);
     }
 }
 
-
 char getCharType(Piece *p)
 {
-    if (p == nullptr) {
+    if (p == nullptr)
+    {
         return ' ';
     }
 
@@ -212,8 +213,8 @@ void Board::setup()
 
     for (int i = 0; i < 8; i++)
     {
-        grid[1][i] = new Pawn(Colour::BLACK, 1, i, false);
-        grid[6][i] = new Pawn(Colour::WHITE, 6, i, false);
+        grid[1][i] = new Pawn(Colour::BLACK, 1, i, false, false);
+        grid[6][i] = new Pawn(Colour::WHITE, 6, i, false, false);
     }
 
     for (int i = 0; i < 8; i++)
--- pawn.cc
+++ pawn.cc
@@ -5,7 +5,7 @@
 #include <vector>
 using namespace std;
 
-Pawn::Pawn(Colour c, int row, int col, bool hasMoved) : Piece(c, Type::PAWN, row, col), hasMoved{hasMoved} {};
+Pawn::Pawn(Colour c, int row, int col, bool hasMoved, bool didDoubleMove) : Piece(c, Type::PAWN, row, col), hasMoved{hasMoved}, didDoubleMove{didDoubleMove} {};
 
 bool Pawn::checkMove(pair<int, int> n, Board *b)
 {
@@ -22,37 +22,56 @@ bool Pawn::checkMove(pair<int, int> n, Board *b)
     int colDiff = n.second - col;
     if (colour == Colour::BLACK)
     {
+        // Pawn takes
         if ((colDiff == 1 || colDiff == -1) && rowDiff == 1)
         {
-            if (b->getPiece(n.first, n.second)->getColour() != colour)
+            // Normal take
+            Piece *p = b->getPiece(row, n.second);
+            if (b->getPiece(n.first, n.second) != nullptr && b->getPiece(n.first, n.second)->getColour() != colour)
             {
+                return true;
+            }
+            // En passant take
+            else if (p != nullptr && row == 4 && p->getColour() != colour && p->getType() == Type::PAWN && static_cast<Pawn *>(p)->didDoubleMove == true)
+            {
+                p = nullptr;
                 return true;
             }
             return false;
         }
-        if (hasMoved)
-        {
-            if (rowDiff == 1 && b->getPiece(n.first, n.second) == nullptr)
-            {
-                return true;
-            }
-            return false;
-        }
+        // Pawn normal move
         else
         {
-            if ((rowDiff == 1 || rowDiff == 2) && b->getPiece(n.first, n.second) == nullptr)
+            if (hasMoved)
             {
-                return true;
+                if (rowDiff == 1 && b->getPiece(n.first, n.second) == nullptr)
+                {
+                    return true;
+                }
+                return false;
             }
-            return false;
+            else
+            {
+                if ((rowDiff == 1 || rowDiff == 2) && b->getPiece(n.first, n.second) == nullptr)
+                {
+                    return true;
+                }
+                return false;
+            }
         }
     }
     else
     {
         if ((colDiff == 1 || colDiff == -1) && rowDiff == -1)
         {
+            Piece *p = b->getPiece(row, n.second);
             if (b->getPiece(n.first, n.second)->getColour() != colour)
             {
+                return true;
+            }
+            else if (p != nullptr && row == 3 && p->getColour() != colour && p->getType() == Type::PAWN && static_cast<Pawn *>(p)->didDoubleMove == true)
+            {
+                p = nullptr;
                 return true;
             }
             return false;
@@ -94,14 +113,14 @@ vector<vector<int>> Pawn::getAttackMoves(Board &b)
     }
     else
     {
-    if ((row - 1 >= 0 && col + 1 < 8) && (b.getPiece(row - 1, col + 1) == nullptr || b.getPiece(row - 1, col + 1)->getColour() != colour))
-    {
-        attackMoves.push_back(vector<int>{row - 1, col + 1, row, col});
-    }
-    if ((row - 1 >= 0 && col - 1 >= 0) && (b.getPiece(row - 1, col - 1) == nullptr || b.getPiece(row - 1, col - 1)->getColour() != colour))
-    {
-        attackMoves.push_back(vector<int>{row - 1, col - 1, row, col});
-    }
+        if ((row - 1 >= 0 && col + 1 < 8) && (b.getPiece(row - 1, col + 1) == nullptr || b.getPiece(row - 1, col + 1)->getColour() != colour))
+        {
+            attackMoves.push_back(vector<int>{row - 1, col + 1, row, col});
+        }
+        if ((row - 1 >= 0 && col - 1 >= 0) && (b.getPiece(row - 1, col - 1) == nullptr || b.getPiece(row - 1, col - 1)->getColour() != colour))
+        {
+            attackMoves.push_back(vector<int>{row - 1, col - 1, row, col});
+        }
     }
     return attackMoves;
 }
