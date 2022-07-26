00da845f763e54b7e696ef29e2d0af3f916754aa
preetmakani <preet.makani@gmail.com>
2022-07-26 00:04:27 -0400

    level4 comp

--- level4computer.h
+++ level4computer.h
@@ -2,11 +2,16 @@
 #define COMPUTER4_H
 #include "player.h"
 #include "board.h"
+#include <vector>
 
 class Computer4 : public Player
 {
+    std::vector<std::vector<int>> startingMoves; 
+    int whiteMoves; 
+    int blackMoves; 
+    int colourNum;
 public:
-    Computer4(Board *b, Colour c): Player(b,c) {};
+    Computer4(Board *b, Colour c);
     bool move(Board *board, std::pair<int, int>, std::pair<int, int>, char) override;
 };
 
